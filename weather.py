1a86c6c7e1304364b34eabdc2b7c1c601e56b93b
Gene Liverman <gene+06108928@technicalissues.us>
2020-02-23 20:34:18 -0500

    Update doc string placement

--- weather.py
+++ weather.py
@@ -75,14 +75,12 @@ def deg_to_compass(degrees):
     return dirs[(val % 16)]
 
 
-"""
-https://darksky.net/dev/docs has lists out what each
-unit is. The method below is just a codified version
-of what is on that page.
-"""
-
-
 def units_decoder(units):
+    """
+    https://darksky.net/dev/docs has lists out what each
+    unit is. The method below is just a codified version
+    of what is on that page.
+    """
     si_dict = {
         'nearestStormDistance': 'Kilometers',
         'precipIntensity': 'Millimeters per hour',
@@ -143,22 +141,21 @@ def get_temperature_letter(unit=config.UNITS):
     return units_decoder(unit)['temperature'].split(' ')[-1][0].upper()
 
 
-"""
-https://darksky.net/dev/docs has this to say about icons:
-icon optional
-A machine-readable text summary of this data point, suitable for selecting an
-icon for display. If defined, this property will have one of the following
-values: clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy,
-partly-cloudy-day, or partly-cloudy-night. (Developers should ensure that a
-sensible default is defined, as additional values, such as hail, thunderstorm,
-or tornado, may be defined in the future.)
-
-Based on that, this method will map the Dark Sky icon name to the name of an
-icon in this project.
-"""
-
 
 def icon_mapping(icon, size):
+    """
+    https://darksky.net/dev/docs has this to say about icons:
+    icon optional
+    A machine-readable text summary of this data point, suitable for selecting an
+    icon for display. If defined, this property will have one of the following
+    values: clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy,
+    partly-cloudy-day, or partly-cloudy-night. (Developers should ensure that a
+    sensible default is defined, as additional values, such as hail, thunderstorm,
+    or tornado, may be defined in the future.)
+
+    Based on that, this method will map the Dark Sky icon name to the name of an
+    icon in this project.
+    """
     if icon == 'clear-day':
         icon_path = 'icons/{}/clear.png'.format(size)
     elif icon == 'clear-night':
@@ -200,12 +197,12 @@ class my_display:
 
     ####################################################################
     def __init__(self):
+        "Ininitializes a new pygame screen using the framebuffer"
         if platform.system() == 'Darwin':
             pygame.display.init()
             driver = pygame.display.get_driver()
             print('Using the {0} driver.'.format(driver))
         else:
-            "Ininitializes a new pygame screen using the framebuffer"
             # Based on "Python GUI in Linux frame buffer"
             # http://www.karoltomala.com/blog/?p=679
             disp_no = os.getenv("DISPLAY")
