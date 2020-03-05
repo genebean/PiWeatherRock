8ec440ede302b0c0ec5aa67fa8f7bf103e7b9651
metaMMA <metammaproject@gmail.com>
2020-03-04 20:48:52 -0600

    Weather info screen code into its own module

---
+++ info.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# BEGIN LICENSE
+# Copyright (c) 2014 Jim Kemp <kemp.jim@gmail.com>
+# Copyright (c) 2017 Gene Liverman <gene@technicalissues.us>
+
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+# END LICENSE
+
+""" Fetches weather reports from Dark Sky for displaying on a screen. """
+
+__version__ = "0.0.12"
+
+###############################################################################
+#   Raspberry Pi Weather Display
+#   Original By: Jim Kemp          10/25/2014
+#   Modified By: Gene Liverman    12/30/2017 & multiple times since
+###############################################################################
+
+# standard imports
+import time
+
+# third party imports
+import pygame
+
+# local imports
+from weather_rock_methods import *
+from weather import *
+
+
+class Info:
+    def get_info(self, last_update_time):
+        return get_forecast(last_update_time)
+
+    def sPrint(self, text, font, x, line_number, text_color):
+        rendered_font = font.render(text, True, text_color)
+        self.screen.blit(rendered_font, (x, self.ymax * 0.075 * line_number))
+
+    def disp_info(self, last_update_time):
+        (in_daylight, day_hrs, day_mins, seconds_til_daylight,
+         delta_seconds_til_dark) = self.daylight(self.weather)
+        # Fill the screen with black
+        self.screen.fill((0, 0, 0))
+        xmin = 10
+        lines = 5
+        line_color = (0, 0, 0)
+        text_color = (255, 255, 255)
+        font_name = "freesans"
+
+        # Draw Screen Border
+        pygame.draw.line(self.screen, line_color,
+                         (xmin, 0), (self.xmax, 0), lines)
+        pygame.draw.line(self.screen, line_color,
+                         (xmin, 0), (xmin, self.ymax), lines)
+        pygame.draw.line(self.screen, line_color,
+                         (xmin, self.ymax), (self.xmax, self.ymax), lines)
+        pygame.draw.line(self.screen, line_color,
+                         (self.xmax, 0), (self.xmax, self.ymax), lines)
+        pygame.draw.line(self.screen, line_color,
+                         (xmin, self.ymax * 0.15),
+                         (self.xmax, self.ymax * 0.15), lines)
+
+        time_height_large = self.time_date_text_height
+        time_height_small = self.time_date_small_text_height
+
+        # Time & Date
+        regular_font = pygame.font.SysFont(
+            font_name, int(self.ymax * time_height_large), bold=1)
+        small_font = pygame.font.SysFont(
+            font_name, int(self.ymax * time_height_small), bold=1)
+
+        hours_and_minutes = time.strftime("%I:%M", time.localtime())
+        am_pm = time.strftime(" %p", time.localtime())
+
+        rendered_hours_and_minutes = regular_font.render(
+            hours_and_minutes, True, text_color)
+        (tx1, ty1) = rendered_hours_and_minutes.get_size()
+        rendered_am_pm = small_font.render(am_pm, True, text_color)
+        (tx2, ty2) = rendered_am_pm.get_size()
+
+        tp = self.xmax / 2 - (tx1 + tx2) / 2
+        self.screen.blit(rendered_hours_and_minutes,
+                         (tp, self.time_date_y_position))
+        self.screen.blit(rendered_am_pm,
+                         (tp + tx1 + 3, self.time_date_small_y_position))
+
+        self.sPrint("A weather rock powered by Dark Sky", small_font,
+                    self.xmax * 0.05, 3, text_color)
+
+        self.sPrint("Sunrise: %s" % self.sunrise_string,
+                    small_font, self.xmax * 0.05, 4, text_color)
+
+        self.sPrint("Sunset:  %s" % self.sunset_string,
+                    small_font, self.xmax * 0.05, 5, text_color)
+
+        text = "Daylight: %d hrs %02d min" % (day_hrs, day_mins)
+        self.sPrint(text, small_font, self.xmax * 0.05, 6, text_color)
+
+        # leaving row 7 blank
+
+        if in_daylight:
+            text = "Sunset in %d hrs %02d min" % stot(delta_seconds_til_dark)
+        else:
+            text = "Sunrise in %d hrs %02d min" % stot(seconds_til_daylight)
+        self.sPrint(text, small_font, self.xmax * 0.05, 8, text_color)
+
+        # leaving row 9 blank
+
+        text = "Weather checked at"
+        self.sPrint(text, small_font, self.xmax * 0.05, 10, text_color)
+
+        text = "    %s" % time.strftime(
+            "%I:%M:%S %p %Z on %a. %d %b %Y ",
+            time.localtime(last_update_time))
+        self.sPrint(text, small_font, self.xmax * 0.05, 11, text_color)
+
+        # Update the display
+        pygame.display.update()
