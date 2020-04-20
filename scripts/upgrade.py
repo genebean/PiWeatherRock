8e28fcefc016351953a6bd0381996db8b73fe058
metaMMA <metammaproject@gmail.com>
2020-04-20 16:30:22 -0500

    Account for additional cases in upgrade script.

--- scripts/upgrade.py
+++ scripts/upgrade.py
@@ -3,86 +3,66 @@ import os
 import re
 from shutil import copyfile
 import socket
+import version
 
 pi_ip = socket.gethostbyname(socket.gethostname() + ".local")
 
-if os.path.exists("config.json"):
-    with open("config.json", "r") as f:
-        old_config = json.load(f)
-    with open("config.json-sample", "r") as f:
-        new_config = json.load(f)
-    old_major, old_minor, old_build = [int(x) for x in
-                                       old_config['version'].split(".")]
-    new_major, new_minor, new_build = [int(x) for x in
-                                       new_config['version'].split(".")]
-    if old_build > new_build:
-        print(f"Error: Current version number is greater than the most "
-              f"recently released version.")
-    elif new_build == old_build:
-        print("No upgrade needed. This is the most recently released version.")
-    else:
-        # Add any new config variables to config.json
-        old_config["version"] = new_config["version"]
-        for key in new_config.keys():
-            if key not in old_config.keys():
-                old_config[key] = new_config[key]
-        with open("config.json", "w") as f:
-            json.dump(old_config, f)
-elif os.path.exists("config.py"):
+if os.path.exists("config.py"):
     print(f"\nImporting current configuration settings.\n\n"
           f"Go to http://{pi_ip}:8888 to view new configuration interface.\n")
-    old_config_dict = {}
-    with open("config.py", "r") as f:
-        old_config = f.read()
-    old_config_dict["ds_api_key"] = re.findall(
-        r"(?<=DS_API_KEY = \').*?(?=')", old_config)[0]
-    old_config_dict["update_freq"] = int(re.findall(
-        r"(?<=DS_CHECK_INTERVAL = )\d+", old_config)[0])
-    old_config_dict["lat"] = float(re.findall(
-        r"(?<=LAT = )[0-9\.-]+", old_config)[0])
-    old_config_dict["lon"] = float(re.findall(
-        r"(?<=LON = )[0-9\.-]+", old_config)[0])
-    old_config_dict["units"] = re.findall(
-        r"(?<=UNITS = \').*?(?=\')", old_config)[0]
-    old_config_dict["lang"] = re.findall(
-        r"(?<=LANG = \').*?(?=\')", old_config)[0]
-    fs_test = re.findall(
-        r"(?<=FULLSCREEN = )(?:True|False)", old_config, re.IGNORECASE)[0]
-    if "t" in [ch for ch in fs_test.lower()]:
-        old_config_dict["fullscreen"] = True
+    import config
+    old_config = {}
+    old_config["ds_api_key"] = config.DS_API_KEY
+    old_config["update_freq"] = int(config.DS_CHECK_INTERVAL)
+    old_config["lat"] = float(config.LAT)
+    old_config["lon"] = float(config.LON)
+    old_config["units"] = config.UNITS
+    old_config["lang"] = config.LANG
+    old_config["fullscreen"] = config.FULLSCREEN
+    old_config["icon_offset"] = float(config.LARGE_ICON_OFFSET)
+    old_config["plugins"] = {}
+    old_config["plugins"]["daily"] = {}
+    old_config["plugins"]["hourly"] = {}
+    old_config["plugins"]["daily"]["enabled"] = True
+    old_config["plugins"]["hourly"]["enabled"] = True
+    if hasattr(config, "DAILY_PAUSE"):
+        old_config["plugins"]["daily"]["pause"] = int(config.DAILY_PAUSE)
     else:
-        old_config_dict["fullscreen"] = False
-    old_config_dict["icon_offset"] = float(re.findall(
-        r"(?<=LARGE_ICON_OFFSET = )[0-9.-]+", old_config)[0])
-
-    daily_pause_list = re.findall(
-        r"(?<=DAILY_PAUSE = )[0-9.-]+", old_config)
-    hourly_pause_list = re.findall(
-        r"(?<=HOURLY_PAUSE = )[0-9.-]+", old_config)
-    info_pause_list = re.findall(
-        r"(?<=INFO_SCREEN_PAUSE = )[0-9.-]+", old_config)
-    info_delay_list = re.findall(
-        r"(?<=INFO_SCREEN_DELAY = )[0-9.-]+", old_config)
-
-    if (daily_pause_list and hourly_pause_list and
-            info_pause_list and info_delay_list):
-        old_config_dict["plugins"] = {}
-        old_config_dict["plugins"]["daily"] = {}
-        old_config_dict["plugins"]["hourly"] = {}
-        old_config_dict["plugins"]["daily"]["pause"] = int(daily_pause_list[0])
-        old_config_dict["plugins"]["daily"]["enabled"] = True
-        old_config_dict["plugins"]["hourly"]["pause"] = int(hourly_pause_list[0])
-        old_config_dict["plugins"]["hourly"]["enabled"] = True
-        old_config_dict["info_pause"] = int(info_pause_list[0])
-        old_config_dict["info_delay"] = int(info_delay_list[0])
-    with open("config.json-sample", "r") as f:
-        new_config_dict = json.load(f)
-    for key in old_config_dict.keys():
-        new_config_dict[key] = old_config_dict[key]
-    with open("config.json", "w") as f:
-        json.dump(new_config_dict, f)
+        old_config["plugins"]["daily"]["pause"] = 60
+    if hasattr(config, "HOURLY_PAUSE"):
+        old_config["plugins"]["hourly"]["pause"] = int(config.HOURLY_PAUSE)
+    else:
+        old_config["plugins"]["hourly"]["pause"] = 60
+    if hasattr(config, "INFO_PAUSE"):
+        old_config["info_pause"] = int(config.INFO_PAUSE)
+    else:
+        old_config["info_pause"] = 300
+    if hasattr(config, "INFO_DELAY"):
+        old_config["info_delay"] = int(config.INFO_DELAY)
+    else:
+        old_config["info_delay"] = 900
     os.remove("config.py")
+elif os.path.exists("config.json"):
+    with open("config.json", "r") as f:
+        old_config = json.load(f)
 else:
-    copyfile("config.json-sample", "config.json")
+    with open("config.json-sample", "r") as f:
+        old_config = json.load(f)
     print(f"\nYou must configure PiWeatherRock.\n\n"
           f"Go to http://{pi_ip}:8888 to configure.\n")
+
+with open("config.json-sample", "r") as f:
+    new_config = json.load(f)
+
+# Add any new config variables
+for key in new_config.keys():
+    if key not in old_config.keys():
+        old_config[key] = new_config[key]
+
+# Remove version key from config file
+if "version" in old_config.keys():
+    del old_config["version"]
+
+with open("config.json", "w") as f:
+    json.dump(old_config, f)
+
