87e484b490e73f912b82a1a7138912776ce92fed
Gene Liverman <gene+06108928@technicalissues.us>
2020-05-03 22:59:50 -0400

    Convert PiWeatherRock to a Python package
    This commit is the first step in packaging PiWeatherRock. It makes
    minimal changes so that a "piweatherrock" package can be created and
    invoked via the cli.

---
+++ setup.py
@@ -0,0 +1,39 @@
+"""Module setup."""
+
+import runpy
+from setuptools import setup, find_namespace_packages, find_packages
+
+PACKAGE_NAME = "piweatherrock"
+version_meta = runpy.run_path("./version.py")
+VERSION = version_meta["__version__"]
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+def parse_requirements(filename):
+    """Load requirements from a pip requirements file."""
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+if __name__ == "__main__":
+    setup(
+        name=PACKAGE_NAME,
+        author="Gene Liverman",
+        author_email="gene@technicalissues.us",
+        version=VERSION,
+        packages=find_packages(),
+        install_requires=parse_requirements("requirements.txt"),
+        python_requires=">=3.7",
+        scripts=['scripts/pwr-ui'],
+        description="Provides forecast data from ClimaCell for PiWeatherRock",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        license='MIT',
+        url='https://github.com/genebean/PiWeatherRock',
+        classifiers=[
+            'License :: OSI Approved :: MIT License',
+        ],
+    )
